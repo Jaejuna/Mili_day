8bb773099a77923964e00029331a2937c6fee310
Jaejuna <anayana9988@hanyang.ac.kr>
2022-01-08 10:24:34 +0000

    22.01.08. working...

--- app.py
+++ app.py
@@ -4,22 +4,9 @@ from sqlalchemy import create_engine, text
 
 app = Flask(__name__) 
 
-app.route('/main', methods = ['GET'])
-def sign_up():
-    user = request.body
-    response = {
-    	'email'    : user['email'],
-        'password' : user['password']
-    }
-    return jsonify(response), 200	
-
 @app.route('/') 
 def hello(): 
-    now = datetime.now() # current date and time
-    date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
-    return render_template('index.html', date_time=date_time)
-
-
+    return render_template('index.html')
     
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', port='8000', debug=True)
