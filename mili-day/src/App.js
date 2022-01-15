e559b5b0c5d014159e8b4d422e168331c13e5dac
Jaejuna <anayana9988@hanyang.ac.kr>
2022-01-15 08:05:52 +0000

    22.01.15. makinng mili day

--- mili-day/src/App.js
+++ mili-day/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+import ClockContainer from "./components/ClockContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+	
+	
+return(
+	<div>
+		<h1>어제의 내일은 오늘이다.</h1>
+	</div>
+	);
 }
 
 export default App;
